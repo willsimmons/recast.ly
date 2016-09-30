c94c39c1b494ab436d9e90179a655a53d9bd274c
willsimmons <willsimmons@gmail.com>
2016-09-30 16:49:27 -0700

    implement YouTube Cat Search
    youtube search written
    component will mount start

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      'key': options[key],
+      'q': 'cats',
+      'videoEmbeddable': true,
+      'type': 'video',
+      'maxResults': 5,
+      'part': 'snippet'
+    },
+    success: (data) => {
+      console.log(data);
+    },
+    error: (error) => {
+      console.error(error);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
