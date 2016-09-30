6b31f724f932dc2611b0c522c3345b8d9d004d93
William Simmons <willsimmons@gmail.com>
2016-09-30 11:58:16 -0700

    extend reactcomponent to make app stateful

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.initialCount
+    };
+  }
 
+  render() {
+    (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer/>
+        </div>
+        <div className="col-md-5">
+          <VideoList/>
+        </div>
+      </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
