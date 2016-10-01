a81534a8ef634986ffcb089eb1354c1635823438
willsimmons <willsimmons@gmail.com>
2016-10-01 11:31:58 -0700

    implement fetch

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,16 +1,22 @@
 var searchYouTube = (options, callback) => {
-  var context = this;
-  $.ajax({
-    url: `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${options.query}&key=${options.key}&maxResults=${options.max}`,
-    success: (data) => {
-      callback(data.items);
-    },
-    error: () => {
+  fetch(`https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${options.query}&key=${options.key}&maxResults=${options.max}`).then(r => r.json())
+    .then(data => callback(data.items))
+    .catch(e => {
+      console.log('Booo');
       callback(exampleVideoData);
-      console.log('failed');
-    }
-  });
+    });
 };
 
 window.searchYouTube = searchYouTube;
+
+  // $.ajax({
+  //   url: `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${options.query}&key=${options.key}&maxResults=${options.max}`,
+  //   success: (data) => {
+  //     callback(data.items);
+  //   },
+  //   error: () => {
+  //     callback(exampleVideoData);
+  //     console.log('failed');
+  //   }
+  // });
 
